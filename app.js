c59522511871047e2c78a1ab8190cdfc1dc38136
Sergey Shytikov <sergeyshytikov@gmail.com>
2022-09-26 11:55:01 +0200

    done ability card to flip

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ const logos = [
   { src: "img/mavic.jpg", name: "mavic" },
   { src: "img/rockMachine.jpg", name: "rock machine" },
 ];
-// TODO: set alt atribute and allow select/open card by tab key
-const section = document.querySelector("section");
+// TODO:  allow select/open card by tab key
 
 const getLogos = () => {
   return [...logos, ...logos].sort(() => Math.random() - 0.5);
 };
 const boardGenerator = () => {
   const board = document.createDocumentFragment();
+  const section = document.querySelector("section");
   section.classList.add("card-board");
   section.setAttribute("tabindex", 0);
   let cardLogos = getLogos();
@@ -23,17 +23,36 @@ const boardGenerator = () => {
     const card = document.createElement("div");
     const face = document.createElement("img");
     const back = document.createElement("img");
+    // card.setAttribute("tabindex", -1);
     card.classList.add("card");
-    card.setAttribute("tabindex", -1);
     face.classList.add("face");
+    face.setAttribute("alt", logo.name);
     back.classList.add("back");
+    back.setAttribute("alt", "card back");
     face.src = logo.src;
-    // back.src = "img/back.jpg";
+    back.src = "img/back.jpg";
     card.appendChild(face);
     card.appendChild(back);
     section.appendChild(card);
   });
   section.appendChild(board);
-  main.appendChild(section);
 };
 boardGenerator();
+
+const cardBoard = document.querySelector(".card-board");
+const cards = document.querySelectorAll(".card");
+console.log(cards);
+
+const flippingCard = (event) => {
+  // console.log(
+  //   event.target.nodeName,
+  //   event.target.innerText,
+  //   event.target.clasName,
+  //   event.target.parentNode
+  // );
+  let parent = event.target.parentNode;
+  if (event.target.nodeName === "IMG") {
+    parent.classList.add("flip");
+  }
+};
+cardBoard.addEventListener("click", flippingCard);

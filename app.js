b13c5e988ca232e7355ee5bbf361a7971118b7f5
Sergey Shytikov <sergeyshytikov@gmail.com>
2022-11-22 21:44:27 +0100

    done arrows and space buttons control

--- app.js
+++ app.js
@@ -15,13 +15,12 @@ const getLogos = () => {
 const board = document.createDocumentFragment();
 const section = document.querySelector("section");
 section.classList.add("card-board");
-section.setAttribute("tabindex", 0);
 let cardLogos = getLogos();
 cardLogos.forEach((logo) => {
   const card = document.createElement("div");
   const face = document.createElement("img");
   const back = document.createElement("img");
-  // card.setAttribute("tabindex", 0);
+  card.setAttribute("tabindex", 0);
   card.classList.add("card");
   card.setAttribute("brand", logo.name);
   face.classList.add("face");
@@ -41,31 +40,32 @@ section.appendChild(board);
 const cardBoard = document.querySelector(".card-board");
 const cards = document.querySelectorAll(".card");
 let isBoardFrezed = false;
-
-const getFlipCard = ({ target }) => {
-  if (target.nodeName !== "IMG") return;
-  if (isBoardFrezed) return;
-  const card = target.parentElement;
-  card.classList.add("flipped");
-  const flippedCards = document.querySelectorAll(".flipped");
-  if (flippedCards.length === 2) {
-    isBoardFrezed = true;
-    let [flippedCardFirst, flippedCardSecond] = flippedCards;
-    let brandFirst = flippedCardFirst.attributes[1].textContent;
-    let brandSecond = flippedCardSecond.attributes[1].textContent;
-    setTimeout(() => {
-      if (brandFirst === brandSecond) {
-        flippedCards.forEach((card) => {
-          card.classList.add("hidden"), card.classList.remove("flipped");
-        });
+const getFlipCard = (event) => {
+  let target = event.target;
+  if (target.nodeName === "IMG" || target.nodeName === "DIV") {
+    if (isBoardFrezed) return;
+    const card = target.nodeName === "DIV" ? target : target.parentElement;
+    card.classList.add("flipped");
+    const flippedCards = document.querySelectorAll(".flipped");
+    if (flippedCards.length === 2) {
+      isBoardFrezed = true;
+      let [flippedCardFirst, flippedCardSecond] = flippedCards;
+      let brandFirst = flippedCardFirst.getAttribute("brand");
+      let brandSecond = flippedCardSecond.getAttribute("brand");
+      setTimeout(() => {
+        if (brandFirst === brandSecond) {
+          flippedCards.forEach((card) => {
+            card.classList.add("hidden"), card.classList.remove("flipped");
+          });
+          isBoardFrezed = false;
+          restartGame();
+        }
         isBoardFrezed = false;
-        restartGame();
-      }
-      isBoardFrezed = false;
-      flippedCards.forEach((card) => {
-        card.classList.remove("flipped");
-      });
-    }, 800);
+        flippedCards.forEach((card) => {
+          card.classList.remove("flipped");
+        });
+      }, 800);
+    }
   }
 };
 const restartGame = () => {
@@ -80,36 +80,43 @@ const restartGame = () => {
 };
 cardBoard.addEventListener("click", getFlipCard);
 
-// TODO:  select card by arrows and flip by space
-// const switchElementsFocus = (event) => {
-//   let target = event.target;
-//   let nextSibling = target.nextSibling;
-//   // let previousSibling = target.previousSibling;
-//   // let fourStepsBack =
-//   //   target.previousSibling.previousSibling.previousSibling.previousSibling;
-//   // let fourStepsForward = target.nextSibling.nextSibling.nextSibling.nextSibling;
-//   switch (event.code) {
-//     case "ArrowUp":
-//       // target.setAttribute("tabindex", -1);
-//       // fourStepsBack.focus();
-//       break;
-//     case "ArrowDown":
-//       // target.setAttribute("tabindex", -1);
-//       // fourStepsForward.focus();
-//       break;
-//     case "ArrowRight":
-//       target.setAttribute("tabindex", -1);
-//       nextSibling.focus();
-//       break;
-//     case "ArrowLeft":
-//       // target.setAttribute("tabindex", -1);
-//       // previousSibling.focus();
-//       break;
-//     case "Space":
-//       // console.log(target.nodeName);
-//       // flippingCard(event);
-//       break;
-//   }
-// };
-
-// cardBoard.addEventListener("keydown", switchElementsFocus);
+const switchElementsFocus = (event) => {
+  let target = event.target;
+  let nextSibling = target.nextSibling;
+  let previousSibling = target.previousSibling;
+  switch (event.code) {
+    case "ArrowUp":
+      if (previousSibling) {
+        let fourStepsBack =
+          target.previousSibling.previousSibling.previousSibling
+            .previousSibling;
+        target.setAttribute("tabindex", -1);
+        fourStepsBack.focus();
+      }
+      break;
+    case "ArrowDown":
+      if (nextSibling) {
+        let fourStepsForward =
+          target.nextSibling.nextSibling.nextSibling.nextSibling;
+        target.setAttribute("tabindex", -1);
+        fourStepsForward.focus();
+      }
+      break;
+    case "ArrowRight":
+      if (nextSibling) {
+        target.setAttribute("tabindex", -1);
+        nextSibling.focus();
+      }
+      break;
+    case "ArrowLeft":
+      if (previousSibling) {
+        target.setAttribute("tabindex", -1);
+        previousSibling.focus();
+      }
+      break;
+    case "Space":
+      getFlipCard(event);
+      break;
+  }
+};
+cardBoard.addEventListener("keydown", switchElementsFocus);
